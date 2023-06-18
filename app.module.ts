f406db2ba155d46d9c7d26bf19d13e0478ed9bcd
StasIvan77 <stasivan77@gmail.com>
2023-06-18 23:43:14 +0300

    Solved

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PasswordStrengthComponent } from './password-strength/password-strength.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PasswordStrengthComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
